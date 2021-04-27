57ce03c7363bfd1be49a9e39ac51e79a8591bd72
Tokai-Robo <82405146+Tokai-Robo@users.noreply.github.com>
2021-04-27 08:04:18 +0700

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -162,7 +162,7 @@ async def upstream(event):
     # Special case for deploy
     if changelog == "" and not force_update:
         await event.edit(
-            "\n`CATUSERBOT is`  **up-to-date**  `with`  "
+            "\n`Tokai-Userbot is`  **up-to-date**  `with`  "
             f"**{UPSTREAM_REPO_BRANCH}**\n"
         )
         return repo.__del__()
